eda7c183921ecc156810d46419498ef906c40fd5
Tanisk1999 <tanishkkhandelwal15@gmail.com>
2024-01-19 18:31:55 +0530

    Added

---
+++ build/server/CMakeFiles/ServerMain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ServerMain.dir/src/main.cpp.o"
+  "ServerMain"
+  "ServerMain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ServerMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,23 +1,21 @@
+#include <iostream>
 #include <dbus-c++-1/dbus-c++/dbus.h>
 #include "/home/kpit/DBus_Code/client/api/sampleproxy.h"
-#include <iostream>
 
 int main() {
     try {
         // Explicitly connect to the D-Bus session bus
-        DBus::Dispatcher* dispatcher = DBus::DefaultDispatcher::create();
-        DBus::Connection* conn = DBus::Connection::create(dispatcher, "tcp:host=localhost,port=12345");
+        DBus::Connection conn = DBus::Connection::SessionBus();
 
         // Create a proxy object
-        com::example::DatabaseManager_proxy proxy;
+        com::example::DatabaseManager_proxy* proxy;
 
         // Use the proxy to call D-Bus methods
-        proxy.Create();
-        proxy.Read();
-        proxy.Update();
-        proxy.Delete();
+        proxy->Create();
+        proxy->Read();
+        proxy->Update();
+        proxy->Delete();
 
-        // Your additional logic here
 
     } catch (DBus::Error &e) {
         std::cerr << "Error: " << e.what() << std::endl;
